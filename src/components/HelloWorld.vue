25b393bd7ddea18f403b57c5ad9b9af4cc07784b
FrancoPlomer <franko.plomer@gmail.com>
2023-06-21 19:55:53 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="card">
+    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+      Como crear una solicitud de mantenimiento online
+    </button>
+    <div class="collapse" id="collapseExample">
+      <div class="card card-body">
+        <iframe src="https://scribehow.com/embed/Como_crear_una_solicitud_de_mantenimiento_online___qnGxrKhSHeerANVI8BqJQ" width="100%" height="640" allowfullscreen frameborder="0"></iframe>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.btn-primary{
+  width: 700px;
+}
+</style>
