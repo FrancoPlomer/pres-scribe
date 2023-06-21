a2d01bcc96799ef0c792bf4ef36f68cbb1476196
FrancoPlomer <franko.plomer@gmail.com>
2023-06-21 20:14:53 -0300

    add

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'scribe-presentation-product',
+  base: 'pres-scribe',
   plugins: [vue()],
 })
